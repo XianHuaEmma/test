2552a3ea722aefbd4b5432a6ab44ff543ec5d8d1
XianHuaEmma <1716642069@qq.com>
2019-09-14 23:18:39 +0800

    创建各种数据类型

--- MavenJavaDemo/src/main/java/com/test/PrometheusDemo.java
+++ MavenJavaDemo/src/main/java/com/test/PrometheusDemo.java
@@ -6,6 +6,8 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.Histogram;
 import io.prometheus.client.exporter.HTTPServer;
 
 public class PrometheusDemo {
@@ -20,21 +22,43 @@ public class PrometheusDemo {
 	    }
 	 public static void submitData() throws InterruptedException {
 	        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-	 
-	        Counter requests = Counter.build()
-	                .name("requests_total_guoyx")
+	 //Counter类型的数据
+	        Counter Countrequests = Counter.build()
+	                .name("requests_total_liubingan")
+	                .labelNames("application")//定义标签的key=labels("application")application
 	                .help("测试")
 	                .register();
+	    //Gauge类型数据
+	        Gauge gaugeRandNum = Gauge.build()
+	        		.name("request_time")
+	        		.labelNames("time")
+	        		.help("Gauge类型测试")
+	        		.register();
+	        
+	      //Histogram类型数据
+	        Histogram histDist = Histogram.build()
+	        		.name("request_time_section")
+	        		.labelNames("section")
+	        		.buckets(2,4,8,12,16,20)//定义多个区间
+	        		.help("Histogram类型数据测试")
+	        		.register();
 	 
 	        int max=20;
 	        int min=1;
 	        Random random = new Random();
 	 
 	        while (true){
-	            TimeUnit.SECONDS.sleep(5);
+	            TimeUnit.SECONDS.sleep(2);
 	            int count = random.nextInt(max)%(max-min+1) + min;
-	            requests.inc(count);
-	            System.out.println(sdf.format(new Date()) + "increase count, current number:" + requests.get());
+	            System.out.println(count);
+	            Countrequests.labels("Count_HTTPServer1243").inc(count);  //包含标签，此时httpServer就是label的val值
+	            Countrequests.labels("Count_HttpServer逐增").inc();
+	            gaugeRandNum.labels("请求时间").set(count);
+	            histDist.labels("响应时间区间").observe(count);//统计随机数，落入的区间，当count小于区间值，则响应的区间值加1；得到的是分布的次数
+	            System.out.println(sdf.format(new Date()) + "increase count, current number:" + Countrequests.labels("Count_HTTPServer1243").get());
+	            System.out.println(sdf.format(new Date()) + "increase count, current number:" + Countrequests.labels("Count_HttpServer逐增").get());
+	            System.out.println(sdf.format(new Date()) + "increase gauge, current time:" + gaugeRandNum.labels("请求时间").get());
+	            
 	        }
 	    }
 
